638ca488d0133f86e8c11e4c0572e567976e8551
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-16 09:01:34 +0200

    check authenticated user and save auth details from Clerk into DB then redirect

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,33 @@
-const NewUserPage = () => {
-  return <div>Hi New-User</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  if (!user) return redirect('/')
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUserPage = async () => {
+  await createNewUser()
+
+  return <div>...loading</div>
 }
 
 export default NewUserPage
