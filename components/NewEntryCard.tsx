da5ba97cb726bcf830bde1dea35a15970fc9862a
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-16 18:29:30 +0200

    creating a new entry in the Journal

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,6 +1,21 @@
+'use client'
+
+import { createNewEntry } from '@/utils/api'
+import { useRouter } from 'next/navigation'
+
 const NewEntryCard = () => {
+  const router = useRouter()
+
+  const handleOnCLick = async () => {
+    const data = await createNewEntry()
+    router.push(`/journal/entry/${data.id}`)
+  }
+
   return (
-    <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+    <div
+      className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
+      onClick={handleOnCLick}
+    >
       <div className="px-4 py-5 sm:p-6">
         <span className="text-3xl">New Entry</span>
       </div>
