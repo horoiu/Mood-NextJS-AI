6f4c6bab413f14b596c7b931aef33545a9ec153f
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-18 22:49:15 +0200

    display Entry details

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,11 +1,45 @@
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
 type EntryPageProps = {
   params: {
     id: string
   }
 }
 
-const EntryPage = ({ params }: EntryPageProps) => {
-  return <div>{params.id}</div>
+export type EntryProps = {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  userId: string
+  content: string
+}
+
+const getEntry = async (id: string) => {
+  const user = await getUserByClerkId()
+
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return (entry as EntryProps) || null
+}
+
+const EntryPage = async ({ params }: EntryPageProps) => {
+  const entry = await getEntry(params.id)
+
+  return (
+    <div>
+      <Editor entry={entry} />
+      {params.id}
+    </div>
+  )
 }
 
 export default EntryPage
