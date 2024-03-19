294f76fc612c73509d45b5d1ec3b0eaac848089d
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-19 09:19:39 +0200

    autosaving edited entries
    this will also install react-autosave package for useAutosave hook

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,12 +2,26 @@
 
 import { useState } from 'react'
 import { EntryProps } from '../app/(dashboard)/journal/[id]/page'
+import { useAutosave } from 'react-autosave'
+import { updateEntry } from '../utils/api'
 
 const Editor = ({ entry }: { entry: EntryProps }) => {
   const [value, setValue] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const update = await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
 
   return (
     <div className="w-full h-full">
+      {isLoading && <div>...loading</div>}
+
       <textarea
         className="w-full h-full p-8 text-xl outline-none"
         value={value}
