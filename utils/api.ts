fc36afca3aff2beb8640f11d05dbe32969b2a9ea
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-19 08:50:32 +0200

    updating Journal Entry

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,21 @@
 const createUrl = (path: string) => window.location.origin + path
 
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(
+    new Request(createUrl(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    })
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  } else {
+    throw new Error('Failed to update entry')
+  }
+}
+
 export const createNewEntry = async () => {
   const res = await fetch(
     new Request(createUrl('/api/journal'), {
