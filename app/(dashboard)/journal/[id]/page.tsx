b03a6c09f4446bff9177d8562fbb5e348541f74f
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-22 08:08:18 +0200

    Sidebar UI

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -34,10 +34,38 @@ const getEntry = async (id: string) => {
 const EntryPage = async ({ params }: EntryPageProps) => {
   const entry = await getEntry(params.id)
 
+  const analysisData = [
+    { name: 'Subject', val: '' },
+    { name: 'Summary', val: '' },
+    { name: 'Mood', val: '' },
+    { name: 'Negative', val: 'False' },
+  ]
+
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
-      {params.id}
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.val}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
