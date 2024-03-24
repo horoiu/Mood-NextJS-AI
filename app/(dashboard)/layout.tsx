c92a9601b12be9d7d25aabc18c0a7c0a83b90e42
Marian Horoiu <marian.horoiu@20robots.tech>
2024-03-24 15:29:04 +0200

    small aside navigation

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,24 @@
-import { UserButton } from '@clerk/nextjs'
 import { ReactNode } from 'react'
+import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-screen h-screen relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-        Mood
+        <div>Mood</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.label} className="px-2 py-6 text-xl">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
 
       <div className="ml-[200px] h-full">
